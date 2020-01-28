3fa7a6ca8d48fda03bbf78587b3afe230f7d3e91
MintK2 <ajwestgerdes@gmail.com>
2020-01-27 21:27:44 -0600

    gihub tracking test

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.amazon.com/Apple-AirPods-Charging-Latest-Model/dp/B07PXGQC1Q/ref=sr_1_1_sspa?crid=2QOXCDI0L1YQ&keywords=airpods&qid=1580093793&sprefix=airpods%2Caps%2C160&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMUlTTzFMMU0wUloyJmVuY3J5cHRlZElkPUEwNzI2NzEwMUIzOFFNNDRQRjg4RSZlbmNyeXB0ZWRBZElkPUExMDA0ODcwREJCVUpMV1FWMk9MJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
 
 app = Flask(__name__)

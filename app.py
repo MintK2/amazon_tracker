b9eeee014513e77f1ce91cbe8688674c953e1ca5
MintK2 <ajwestgerdes@gmail.com>
2020-01-29 21:04:32 -0600

    added form

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
+    if request.method == 'POST':
+        url = request.form['url']
     return render_template('index.html')
 
 if __name__ == '__main__':
